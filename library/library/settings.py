ac44c042c760600c3ecbd89475053d9eba42c948
Maks Tishchuk <makstischuk@gmail.com>
2021-07-04 16:25:58 +0300

    Added filter, search and ordering. +Tests

--- library/library/settings.py
+++ library/library/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
+    'django_filters',
 
     'store',
 ]
@@ -136,5 +138,8 @@ SITE_ID = 1
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
 }
